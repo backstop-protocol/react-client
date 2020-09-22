a9eab0e81ce4a2cba991911428e78d94bb0bdb57
gutzy <ikoosh@gmail.com>
2020-09-22 22:52:09 +0300

    removed outline, added 0 fallback to numm number formatter

--- src/lib/Utils.js
+++ src/lib/Utils.js
@@ -1,1 +1,1 @@
-export const numm = (v,decPoint = null, max = Infinity) => Math.min(max, decPoint !== null ? parseFloat(v).toFixed(decPoint) : parseFloat(v).toFixed(2)*1);
+export const numm = (v,decPoint = null, max = Infinity) => {const r = Math.min(max, decPoint !== null ? parseFloat(v).toFixed(decPoint) : parseFloat(v).toFixed(2)*1); return (isNaN(r))?0:r}
