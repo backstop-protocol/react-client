f0935ffee01be7991e9a1fa6f9018ff771ee7b3a
gutzy <ikoosh@gmail.com>
2020-09-24 09:18:18 +0300

    typo fix

--- src/components/LoadingRing.js
+++ src/components/LoadingRing.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function LoadingRing() {
     return (
-        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+        <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
     )
 }
