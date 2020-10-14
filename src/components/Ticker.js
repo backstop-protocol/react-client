cc71ba9f10d41e4f33269d4d74fcf27bfdfcba07
AshfiAnsari <ashfiansari1417@gmail.com>
2020-10-14 11:33:22 +0530

    changes in the text of user rating

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,43 +1,68 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 export default class Ticker extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.state = {
+      value: "0",
 
-        this.state = {
-            value : '0',
-            changes : [],
+      changes: [],
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      (this.props.value && !this.state.value) ||
+      this.state.value !== this.props.value
+    ) {
+      let changes = [];
+      if (prevState.value) {
+        const old = prevState.value.toString(),
+          notold = this.props.value.toString();
+        for (let c = 0; c < old.length; c++) {
+          if (notold[c] !== old[c]) changes.push(c);
         }
+      }
+      if (this.props.value !== "0" && this.props.value !== "")
+        this.setState({ value: this.props.value, changes });
+      setTimeout(() => this.setState({ changes: [] }), 300);
     }
+  }
 
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.value && !this.state.value || this.state.value !== this.props.value) {
-            let changes = [];
-            if (prevState.value) {
-                const old = prevState.value.toString(), notold = this.props.value.toString();
-                for (let c = 0; c < old.length; c++) {
-                    if (notold[c] !== old[c]) changes.push(c);
-                }
+  render() {
+    let { value, changes } = this.state;
+    let { primary } = this.props;
+    primary = primary ? primary : 6666;
+
+    value = value ? value.toString().split("") : ["0"];
+
+    console.log("length.........", value.length);
+
+    return (
+      <span className="ticker">
+        {value.map((n, index) => (
+          <span
+            key={index}
+            className={
+              n === "."
+                ? " dot"
+                : "" +
+                  (n === "1" ? " one" : "") +
+                  (value.length > 6
+                    ? index < primary
+                      ? " primary-small"
+                      : "dot-small"
+                    : index < primary
+                    ? " primary"
+                    : "") +
+                  (changes.indexOf(index) > -1 ? " in" : "")
             }
-            if (this.props.value !== '0' && this.props.value !== '')
-            this.setState({value : this.props.value, changes});
-            setTimeout(() => this.setState({changes: []}), 300);
-        }
-
-    }
-
-    render() {
-        let {value, changes} = this.state;
-        let {primary} = this.props;
-        primary = primary ? primary : 6666;
-
-        value = value ? value.toString().split('') : ['0'];
-
-        return (
-            <span className="ticker">
-                {value.map((n, index) => <span key={index} className={n==='.'?' dot':'' + (n==='1'?' one':'') + (index<primary?' primary':'') + (changes.indexOf(index)>-1?' in':'')}>{n}</span>)}
-            </span>
-        )
-    }
+          >
+            {n}
+          </span>
+        ))}
+      </span>
+    );
+  }
 }
