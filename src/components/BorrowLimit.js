2ae0a42270ef5b907555507305310497dc83f359
AshfiAnsari <ashfiansari1417@gmail.com>
2020-10-14 14:46:55 +0530

    layout fix of global stats

--- src/components/BorrowLimit.js
+++ src/components/BorrowLimit.js
@@ -1,26 +1,41 @@
-import React, {Component} from "react";
-import {numm} from "../lib/Utils";
+import React, { Component } from "react";
+import { numm } from "../lib/Utils";
 
 export default class BorrowLimit extends Component {
+  render() {
+    const { userInfo } = this.props;
 
-
-    render() {
-
-        const {userInfo} = this.props;
-
-        return (
-            <div className="borrow-limit">
-                <h3>Borrow Limit</h3>
-                <div className="limit-bar">
-                    <label>{userInfo?numm(userInfo.bCdpInfo.daiDebt):0} DAI</label>
-                    <div className="limit-bar-inner">
-                        <div className="limit-bar-track" style={{width: (userInfo && userInfo.bCdpInfo.maxDaiDebt ? (userInfo.bCdpInfo.daiDebt / userInfo.bCdpInfo.maxDaiDebt * 100):0)+"%"}}>
-                            <span>{(userInfo && userInfo.bCdpInfo.maxDaiDebt?numm(userInfo.bCdpInfo.daiDebt / userInfo.bCdpInfo.maxDaiDebt * 100):0)+"%"}</span>
-                        </div>
-                    </div>
-                    <label>{userInfo?numm(userInfo.bCdpInfo.maxDaiDebt):0} DAI</label>
-                </div>
+    return (
+      <div className="borrow-limit">
+        <h3 style={{ fontWeight: 500, color: "#0b0412" }}>Borrow Limit</h3>
+        <div className="limit-bar">
+          <label>{userInfo ? numm(userInfo.bCdpInfo.daiDebt) : 0} DAI</label>
+          <div className="limit-bar-inner">
+            <div
+              className="limit-bar-track"
+              style={{
+                width:
+                  (userInfo && userInfo.bCdpInfo.maxDaiDebt
+                    ? (userInfo.bCdpInfo.daiDebt /
+                        userInfo.bCdpInfo.maxDaiDebt) *
+                      100
+                    : 0) + "%",
+              }}
+            >
+              <span>
+                {(userInfo && userInfo.bCdpInfo.maxDaiDebt
+                  ? numm(
+                      (userInfo.bCdpInfo.daiDebt /
+                        userInfo.bCdpInfo.maxDaiDebt) *
+                        100
+                    )
+                  : 0) + "%"}
+              </span>
             </div>
-        )
-    }
+          </div>
+          <label>{userInfo ? numm(userInfo.bCdpInfo.maxDaiDebt) : 0} DAI</label>
+        </div>
+      </div>
+    );
+  }
 }
