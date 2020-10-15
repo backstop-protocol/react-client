07053fc641e88063551aff7e57952c5567e6b45a
AshfiAnsari <ashfiansari1417@gmail.com>
2020-10-15 13:00:32 +0530

    changes of user rating numbers

--- src/components/GlobalStats.js
+++ src/components/GlobalStats.js
@@ -1,128 +1,3 @@
-// import React, { Component } from "react";
-// import InfoIcon from "../assets/i-icon.svg";
-// import DollarIcon from "../assets/dollar-icon.svg";
-// import Pulser from "./Pulser";
-// import Ticker from "./Ticker";
-// import Tooltip from "./Tooltip";
-// import { numm } from "../lib/Utils";
-
-// const ratingFactor = 300000;
-
-// export default class GlobalStats extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       currentRating: null,
-//       ratingProgress: null,
-//       ratingInterval: null,
-//     };
-//   }
-
-//   componentDidUpdate(prevProps, prevState, ss) {
-//     const { userInfo } = this.props;
-//     if (!userInfo) return;
-
-//     if (!this.state.currentRating) {
-//       clearInterval(this.state.ratingInterval);
-//       const interval = setInterval(this.updateUserRating, 3000);
-
-//       this.setState({
-//         ratingProgress:
-//           parseFloat(userInfo.userRatingInfo.userRatingProgressPerSec) /
-//           ratingFactor,
-//       });
-//       this.setState({
-//         currentRating: parseFloat(
-//           userInfo.userRatingInfo.userRating / ratingFactor
-//         ).toFixed(7),
-//       });
-//     }
-
-//     const currRatingProgress =
-//       parseFloat(userInfo.userRatingInfo.userRatingProgressPerSec) /
-//       ratingFactor;
-//     if (this.state.ratingProgress !== currRatingProgress) {
-//       clearInterval(this.state.ratingInterval);
-//       const interval = setInterval(this.updateUserRating, 3000);
-
-//       this.setState({ ratingProgress: currRatingProgress });
-//       this.setState({
-//         currentRating: parseFloat(
-//           userInfo.userRatingInfo.userRating / ratingFactor
-//         ).toFixed(7),
-//       });
-//     }
-//   }
-
-//   updateUserRating = () => {
-//     const currentRating = this.state.currentRating;
-//     const nextRating = parseFloat(
-//       currentRating * 1 + this.state.ratingProgress * 3
-//     );
-//     this.setState({ currentRating: nextRating.toFixed(7) });
-//   };
-
-//   render() {
-//     const { userInfo } = this.props;
-//     const { currentRating } = this.state;
-
-//     return (
-//       <div className="global-stats even">
-//         <div className="stats">
-//           <div className="left">
-//             <h2>
-//               Jar Balance
-//               <span className="tooltip-container">
-//                 <Tooltip>Jar Balance</Tooltip>
-//                 <img className="info-icon" src={InfoIcon} />
-//               </span>
-//             </h2>
-//             <div className="value">
-//               $
-//               <Ticker
-//                 value={
-//                   userInfo
-//                     ? userInfo.userRatingInfo.jarBalance === 0
-//                       ? 10000
-//                       : userInfo.userRatingInfo.jarBalance *
-//                         userInfo.miscInfo.spotPrice
-//                     : 10000
-//                 }
-//               />
-//             </div>
-//           </div>
-//           <div className="right">
-//             <h2>
-//               User Rating
-//               <span className="tooltip-container">
-//                 <Tooltip>
-//                   <small>Total Rating</small>
-//                   <h3>
-//                     {numm(
-//                       userInfo
-//                         ? userInfo.userRatingInfo.totalRating / ratingFactor
-//                         : 0,
-//                       2
-//                     )}
-//                   </h3>
-//                 </Tooltip>
-//                 <img className="info-icon" src={InfoIcon} />
-//               </span>
-//             </h2>
-//             <div className="value">
-//               <Ticker value={userInfo ? currentRating : 0} primary={5} />
-//             </div>
-//           </div>
-//         </div>
-//         <div className="image-container">
-//           <Pulser />
-//           <img src={DollarIcon} className="dollar-icon floating centered" />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
 import React, { Component } from "react";
 import InfoIcon from "../assets/i-icon.svg";
 import DollarIcon from "../assets/dollar-icon.svg";
@@ -137,13 +12,47 @@ export default class GlobalStats extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentRating: 0,
-      ratingProgress: 10.00234,
+      currentRating: null,
+      ratingProgress: null,
       ratingInterval: null,
     };
   }
 
-  componentDidUpdate(prevProps, prevState, ss) {}
+  componentDidUpdate(prevProps, prevState, ss) {
+    const { userInfo } = this.props;
+    if (!userInfo) return;
+
+    if (!this.state.currentRating) {
+      clearInterval(this.state.ratingInterval);
+      const interval = setInterval(this.updateUserRating, 3000);
+
+      this.setState({
+        ratingProgress:
+          parseFloat(userInfo.userRatingInfo.userRatingProgressPerSec) /
+          ratingFactor,
+      });
+      this.setState({
+        currentRating: parseFloat(
+          userInfo.userRatingInfo.userRating / ratingFactor
+        ).toFixed(7),
+      });
+    }
+
+    const currRatingProgress =
+      parseFloat(userInfo.userRatingInfo.userRatingProgressPerSec) /
+      ratingFactor;
+    if (this.state.ratingProgress !== currRatingProgress) {
+      clearInterval(this.state.ratingInterval);
+      const interval = setInterval(this.updateUserRating, 3000);
+
+      this.setState({ ratingProgress: currRatingProgress });
+      this.setState({
+        currentRating: parseFloat(
+          userInfo.userRatingInfo.userRating / ratingFactor
+        ).toFixed(7),
+      });
+    }
+  }
 
   updateUserRating = () => {
     const currentRating = this.state.currentRating;
@@ -156,10 +65,7 @@ export default class GlobalStats extends Component {
   render() {
     const { userInfo } = this.props;
     const { currentRating } = this.state;
-    if (this.state.ratingInterval == null) {
-      const interval = setInterval(this.updateUserRating, 3000);
-      this.setState({ ratingInterval: interval });
-    }
+
     return (
       <div className="global-stats even">
         <div className="stats">
@@ -173,7 +79,16 @@ export default class GlobalStats extends Component {
             </h2>
             <div className="value">
               $
-              <Ticker value={10000} />
+              <Ticker
+                value={
+                  userInfo
+                    ? userInfo.userRatingInfo.jarBalance === 0
+                      ? 10000
+                      : userInfo.userRatingInfo.jarBalance *
+                        userInfo.miscInfo.spotPrice
+                    : 10000
+                }
+              />
             </div>
           </div>
           <div className="right">
@@ -182,13 +97,20 @@ export default class GlobalStats extends Component {
               <span className="tooltip-container">
                 <Tooltip>
                   <small>Total Rating</small>
-                  <h3>{numm(0)}</h3>
+                  <h3>
+                    {numm(
+                      userInfo
+                        ? userInfo.userRatingInfo.totalRating / ratingFactor
+                        : 0,
+                      2
+                    )}
+                  </h3>
                 </Tooltip>
                 <img className="info-icon" src={InfoIcon} />
               </span>
             </h2>
             <div className="value">
-              <Ticker value={currentRating} primary={5} />
+              <Ticker value={userInfo ? currentRating : 0} primary={5} />
             </div>
           </div>
         </div>
