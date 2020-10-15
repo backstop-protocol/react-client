04b446c2ec3d92e582f86d6b2426478f33449ba6
AshfiAnsari <ashfiansari1417@gmail.com>
2020-10-15 20:35:59 +0530

    ticker spaces and tabs fix

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -1,68 +1,65 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 export default class Ticker extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      value: "0",
+    constructor(props) {
+        super(props);
 
-      changes: [],
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      (this.props.value && !this.state.value) ||
-      this.state.value !== this.props.value
-    ) {
-      let changes = [];
-      if (prevState.value) {
-        const old = prevState.value.toString(),
-          notold = this.props.value.toString();
-        for (let c = 0; c < old.length; c++) {
-          if (notold[c] !== old[c]) changes.push(c);
+        this.state = {
+            value : '0',
+            changes : [],
         }
-      }
-      if (this.props.value !== "0" && this.props.value !== "")
-        this.setState({ value: this.props.value, changes });
-      setTimeout(() => this.setState({ changes: [] }), 300);
     }
-  }
 
-  render() {
-    let { value, changes } = this.state;
-    let { primary } = this.props;
-    primary = primary ? primary : 6666;
-
-    value = value ? value.toString().split("") : ["0"];
-
-    return (
-      <span className="ticker">
-        {value.map((n, index) => (
-          <span
-            key={index}
-            className={
-              n === "."
-                ? "dot"
-                : "" +
-                  (n === "1" ? (value.length > 7 ? "s-one" : " one") : "") +
-                  (value.length > 7
-                    ? index < primary
-                      ? " s-primary"
-                      : n === "1"
-                      ? "s-one small fade-small-one"
-                      : "small"
-                    : index < primary
-                    ? " primary"
-                    : "") +
-                  (changes.indexOf(index) > -1 ? " in" : "")
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.value && !this.state.value || this.state.value !== this.props.value) {
+            let changes = [];
+            if (prevState.value) {
+                const old = prevState.value.toString(), notold = this.props.value.toString();
+                for (let c = 0; c < old.length; c++) {
+                    if (notold[c] !== old[c]) changes.push(c);
+                }
             }
-          >
-            {n}
+            if (this.props.value !== '0' && this.props.value !== '')
+            this.setState({value : this.props.value, changes});
+            setTimeout(() => this.setState({changes: []}), 300);
+        }
+
+    }
+
+    render() {
+        let {value, changes} = this.state;
+        let {primary} = this.props;
+        primary = primary ? primary : 6666;
+
+        value = value ? value.toString().split('') : ['0'];
+
+        return (
+          <span className="ticker">
+            {value.map((n, index) => (
+              <span
+                key={index}
+                className={
+                  n === "."
+                    ? "dot"
+                    : "" +
+                      (n === "1" ? (value.length > 7 ? "s-one" : " one") : "") +
+                      (value.length > 7
+                        ? index < primary
+                          ? " s-primary"
+                          : n === "1"
+                          ? "s-one small fade-small-one"
+                          : "small"
+                        : index < primary
+                        ? " primary"
+                        : "") +
+                      (changes.indexOf(index) > -1 ? " in" : "")
+                }
+              >
+                {n}
+              </span>
+            ))}
           </span>
-        ))}
-      </span>
-    );
-  }
+        );
+    }
 }
