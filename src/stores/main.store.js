58b31931a1a234d166c60225617682b8f5cc5297
shmuel-web <shmuel.disraeli@gmail.com>
2020-12-03 13:33:05 +0200

    cleanup

--- src/stores/main.store.js
+++ src/stores/main.store.js
@@ -25,22 +25,14 @@ class MainStore {
     async fetchData () {
         try{
             const web3 = new Web3('https://bp-api.bprotocol.workers.dev')
-            // const web3 = new Web3('https://bp-api.bprotocol.workers.dev/wrapper')
             let info = await B.getUserInfo(web3, '1', '0x0000000000000000000000000000000000000000')
-            debugger
+
             this.originalInfoResponse = info
             info = ApiHelper.Humanize(info, web3);
-            // this is a patch to avoid the wired response object being parsed as an array
-            const fixedInfoObj = {}
-            Object.keys(info).forEach(key => {
-                if(isNaN(key)){
-                    fixedInfoObj[key] = info[key]
-                }
-            })
-            this.generalInfo = fixedInfoObj
-            
+           
             this.jarBalanceEth = parseFloat(info.userRatingInfo.jarBalance).toFixed(1);
             this.jarBalanceUsd = parseFloat(info.userRatingInfo.jarBalance * info.miscInfo.spotPrice).toFixed(0)
+            
         }catch (err){
             console.error('failed to fatch general stats')
         }
