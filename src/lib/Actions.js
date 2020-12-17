d8caf0b2630af4af1f310c731a7466471eeffd7a
shmuel-web <shmuel.disraeli@gmail.com>
2020-12-17 16:13:53 +0200

    indentations and spaces

--- src/lib/Actions.js
+++ src/lib/Actions.js
@@ -54,7 +54,7 @@ export function setUserInfo(u, w3, id, info, orgInfo) {
 }
 
 export function checkForUnlockedEth() {
-    if(userInfo && userInfo.bCdpInfo && userInfo.bCdpInfo.unlockedEth){
+    if(userInfo && userInfo.bCdpInfo && userInfo.bCdpInfo.unlockedEth) {
         const eth = userInfo.bCdpInfo.unlockedEth.toFixed(4)
         const msg = `your ETH vault auction is completed. you have ${eth} ETH to claim`
         const btn = 'claim'
@@ -65,11 +65,11 @@ export function checkForUnlockedEth() {
 
 export async function claimUnlockedEth() {
     try{
-    EventBus.$emit('app-alert', 'claim unlocked ETH pending')
-    const action = B.claimUnlockedCollateral(web3, networkId, userInfo.proxyInfo.userProxy, userInfo.bCdpInfo.cdp, originalUserInfo.bCdpInfo.unlockedEth)
-    await ApiAction(action, user, web3, 0)
-    refreshUserInfo()
-    }catch (err){
+        EventBus.$emit('app-alert', 'claim unlocked ETH pending')
+        const action = B.claimUnlockedCollateral(web3, networkId, userInfo.proxyInfo.userProxy, userInfo.bCdpInfo.cdp, originalUserInfo.bCdpInfo.unlockedEth)
+        await ApiAction(action, user, web3, 0)
+        refreshUserInfo()
+    } catch (err){
         checkForUnlockedEth()
         throw err
     }
