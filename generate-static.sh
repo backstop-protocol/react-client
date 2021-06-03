38e1d8aeccedd61266271d670d108d2bc14cefc4
shmuel-web <shmuel.disraeli@gmail.com>
2021-06-03 12:14:19 +0300

    generate-static

---
+++ generate-static.sh
@@ -0,0 +1,7 @@
+cd build
+mkdir app && cp index.html app
+mkdir maker && cp index.html maker
+mkdir compound && cp index.html compound
+mkdir faq && cp index.html faq
+mkdir risk && cp index.html risk
+mkdir terms && cp index.html terms
