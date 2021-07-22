3d8b6dcaf1a83ec8fe37140e3d0e95de8dc7347d
shmuel <shmuel@shmuels-MacBook-Pro.local>
2021-07-22 12:21:50 +0300

    added liquity

--- generate-static.sh
+++ generate-static.sh
@@ -2,6 +2,7 @@ cd build
 mkdir app && cp index.html app
 mkdir maker && cp index.html maker
 mkdir compound && cp index.html compound
+mkdir liquity && cp index.html liquity
 mkdir faq && cp index.html faq
 mkdir risk && cp index.html risk
 mkdir terms && cp index.html terms
