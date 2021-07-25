eb02435b6a90e7f9a47bec419619b38535aa7bca
shmuel <shmuel@shmuels-MacBook-Pro.local>
2021-07-25 18:09:19 +0300

    fixing the iframe

--- src/containers/Liquity.js
+++ src/containers/Liquity.js
@@ -51,7 +51,7 @@ class Liquity extends Component {
     console.log("comp userInfoUpdate ", userInfoUpdate)
     return (
       <iframe 
-        src="https://liquity-app.bprotocol.workers.dev"
+        src="https://liquity.bprotocol.org"
         width="100%"
         height="100%"
         frameBorder="0"
