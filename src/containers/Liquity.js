b2b324d26b180ea78f3a111a21f778b9cf87788d
shmuel <shmuel@shmuels-MacBook-Pro.local>
2021-07-26 11:41:28 +0300

    cleanUp

--- src/containers/Liquity.js
+++ src/containers/Liquity.js
@@ -1,24 +1,8 @@
 
 
 import React, { Component } from "react";
-import logo from "../assets/compound-full-logo.svg";
-import Etherium from "../assets/etherium.svg";
-import Header2 from "../components/Header2";
-import { doApiAction } from "../lib/Actions";
-import compoundStore from "../stores/compound.store"
 import {observer} from "mobx-react"
-import styled from "styled-components"
-import Flex, {FlexItem} from "styled-flex-component";
-import BalanceBox from "../components/compound-components/BalanceBox"
-import CoinListBox from "../components/compound-components/CoinListBox"
-import {CoinStatusEnum} from "../lib/compound.util"
-import {device} from "../screenSizes";
 import routerStore from "../stores/router.store"
-import userStore from "../stores/user.store"
-
-const Overides = styled.div`
-    margin-bottom: 100px;
-`
 
 class Liquity extends Component {
 
@@ -28,22 +12,6 @@ class Liquity extends Component {
 
   componentDidMount() {
     routerStore.setRouteProps(this.props.history) 
-    compoundStore.getUserInfo()
-  }
-
-  getList(coinStatusToShow){
-    const items = compoundStore.coinList.filter(coinAddress=> {
-        const coin = compoundStore.coinsInTx[coinAddress] || compoundStore.coinMap[coinAddress] // preserve state until tx is finished and UI is ready to dispaly new coin state
-        if(!coin.symbol){
-            return false
-        }
-        return coin.isCoinStatus(coinStatusToShow)
-    })
-    .map(coinAddress => {
-        const coin = compoundStore.coinsInTx[coinAddress] || compoundStore.coinMap[coinAddress]
-        return coin
-    }) 
-    return items     
   }
 
   render() {
