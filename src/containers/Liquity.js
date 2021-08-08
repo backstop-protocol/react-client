128484c926a3fc94fd929e7262fc60ee6726c39f
Shmuel Disraeli <shmuel-web@users.noreply.github.com>
2021-08-08 10:45:16 +0300

    Update Liquity.js

--- src/containers/Liquity.js
+++ src/containers/Liquity.js
@@ -15,7 +15,7 @@ class Liquity extends Component {
   render() {
     return (
       <iframe 
-        src="/liquity-app"
+        src="/liquity-app/"
         width="100%"
         height="100%"
         frameBorder="0"
