0c6da49ac908fa3e0619077a4e83ab46cc308d0c
shmuel <shmuel@shmuels-MacBook-Pro.local>
2021-10-03 08:46:42 +0300

    temp

--- tests/e2e/specs/2_maker-gem-spec.js
+++ tests/e2e/specs/2_maker-gem-spec.js
@@ -28,6 +28,13 @@ describe('Wallet tests', () => {
       cy.wait(4 * 1000)
       cy.confirmMetamaskTransaction()
       cy.wait(4 * 1000)
+      cy.wait(4 * 1000)
+      cy.get('#step-1').contains('Done')
+    })
+
+    it('Create a proxy account done', () => {
+      cy.wait(4 * 1000)
+      cy.wait(4 * 1000)
       cy.get('#step-1').contains('Done')
     })
 
@@ -37,6 +44,13 @@ describe('Wallet tests', () => {
       cy.wait(4 * 1000)
       cy.confirmMetamaskPermissionToSpend()
       cy.wait(4 * 1000)
+      cy.wait(4 * 1000)
+      cy.get('#step-2').contains('Done')
+    })
+
+    it('Unlocks gem done', () => {
+      cy.wait(4 * 1000)
+      cy.wait(4 * 1000)
       cy.get('#step-2').contains('Done')
     })
 
@@ -45,6 +59,13 @@ describe('Wallet tests', () => {
       cy.wait(4 * 1000)
       cy.wait(4 * 1000)
       cy.confirmMetamaskTransaction()
+      cy.wait(4 * 1000)
+      cy.wait(4 * 1000)
+      cy.get('#WBTC-box').find('.currency-value').find('p').should('have.text', '1.00000 WBTC')
+      cy.get('#WBTC-box').find('.currency-value').find('small').should('have.text', '49284.04 USD')
+    })
+    it('validats the gem deposit', ()=>{
+      cy.wait(4 * 1000)
       cy.wait(4 * 1000)
       cy.get('#WBTC-box').find('.currency-value').find('p').should('have.text', '1.00000 WBTC')
       cy.get('#WBTC-box').find('.currency-value').find('small').should('have.text', '49284.04 USD')
